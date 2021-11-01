faf8d9b31b5c0e7abf554a63b043a43fb8e6584a
Toan Phan <toanphan@berkeley.edu>
2021-11-01 12:23:14 -0700

    first commit
    open-source chrome extension to notify the user if the website found in a data breach

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+/*global chrome*/
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+import {Fragment, useEffect, useState} from 'react';
 
 function App() {
+  const [site, setSite] = useState("");
+  const [breach, setBreach] = useState([]);
+  
+  // function to set the current tab's website to site
+  const getCurrentTab = async () => {
+    try {
+      let queryOptions = {active: true, currentWindow: true };
+      let [tab] = await chrome.tabs.query(queryOptions);
+      var url = new URL(tab.url)
+      var domain = url.hostname
+      if (domain.startsWith('www')) {
+        domain = domain.substring(4, domain.length); 
+      }
+      setSite(domain);
+      return domain;
+    } catch(err) {
+      console.log(err.message);
+    }
+  }
+
+  const baseURL = "https://haveibeenpwned.com/api/v3/breaches/?domain=";
+
+  // function to find if the website found in a data breach
+  // in Have I Been Pwned database.
+  const findBreach = async () => {
+    let domainName = await getCurrentTab();
+    const url = baseURL + domainName; 
+    try {
+        const response = await fetch(url); 
+        const jsonData = await response.json();
+        setBreach(jsonData);
+    } catch(err) {
+        console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    findBreach();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <Fragment>
+      <div className="App">
+        <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h2>Pwned Notifier</h2>
+        </div>
+        <h2>{breach.length > 0 && `${site} was found in a data breach. :( `}</h2>
+        <h2>{breach.length === 0 && `${site} was NOT found in a data breach. :) `}</h2>
+      </div>
+    </Fragment>
   );
 }
 
